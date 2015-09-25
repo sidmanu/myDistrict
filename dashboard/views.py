eb07f85702d706c66b923014b382e0492ea78833
sidmanu <siddharthmanu@gmail.com>
2015-09-25 15:44:58 +0530

    initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
+from django.utils.html import format_html
+import sys, traceback
+
+from dashboard.models import *
+
+def get_sidebar_context():
+    context = {}
+    return context
+
+def my_render(request, template, context):
+	template = 'dashboard/' + template
+	return render(request, template, context)
+
+
+def index(request):
+	context = get_sidebar_context()
+	return my_render(request, 'index.html', context)
+	
